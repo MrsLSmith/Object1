2b0644cec7f4eafc03a007a45aebcc21e996d6d6
Parker <PFranz@ccsuvt.org>
2016-02-18 10:39:01 -0500

    Changes

--- superheroes.js
+++ superheroes.js
@@ -1,13 +1,6 @@
-var superheroes = [{
-  Name: "Incredible Hulk",
-  Color: "Green",
-  Superpower: "Strength"
-}, {
-  Name: "Superman",
-  Color: "Blue",
-  Superpower: "Flying"
-}, {
-  Name: "Hawkeye",
-  Color: "Purple",
-  Superpower: "Archery"
-}];
+var HeroNames= ["Hawkeye","Hulk","Spiderman"] 
+
+Function List() {
+  
+  
+}
