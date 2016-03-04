5381542a0463fb6dae40ff3bd0c10f07a4ed4a76
Parker <PFranz@ccsuvt.org>
2016-03-04 09:55:25 -0500

    Changes

--- superheroes.js
+++ superheroes.js
@@ -1,6 +1,28 @@
-var HeroNames= ["Hawkeye","Hulk","Spiderman"] 
+var superheroes = [{
+  Name: "Incredible Hulk",
+  Color: "Green",
+  Superpower: "Strength"
+}, {
+  Name: "Superman",
+  Color: "Blue",
+  Superpower: "Flying"
+}, {
+  Name: "Hawkeye",
+  Color: "Purple",
+  Superpower: "Archery"
+}]; 
 
-Function List() {
+function List() {
+  var HeroName = "";  
+  for(i=0; i<superheroes.length; i+=1) {
+    HeroName +="<p>" + superheroes[i].Name +"</p>";
+
+    
+    
+  }
+
+  var output = document.getElementById ("HeroList");
   
+  output.innerHTML = HeroName;
   
 }
